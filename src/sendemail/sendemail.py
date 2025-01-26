872c1b11d29cb2d7c2c87651b4ef58a361422796
Lucas Leme Cardoso Ferreira <54031977+LucasLemeCF@users.noreply.github.com>
2025-01-26 11:54:05 -0300

    Corrigido testes untarios

--- src/sendemail/sendemail.py
+++ src/sendemail/sendemail.py
@@ -1,67 +1,42 @@
 import sys
-import json
-import requests
-import os
-import logging
-
-# Configuração básica do logging
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-logger = logging.getLogger()
-
 sys.path.append('/opt/bin/')
 
+import json
+import requests
+import logging
+import boto3
+import os
+
 email_sender = "videoframeprofiap@gmail.com"
+url_smtp = "https://api.brevo.com/v3/smtp/email"
+queue_url = 'https://sqs.${var.aws_region}.amazonaws.com/${data.aws_caller_identity.current.account_id}/${var.sqs_queue_name}'
 email_api_key = os.environ['email_api_key']
 
-url_smtp = "https://api.brevo.com/v3/smtp/email"
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+sqs_client = boto3.client('sqs', region_name=os.environ["aws_region"])
 
 def lambda_handler(event, context):
-    logger.info("Iniciando o processamento do evento...")
-
-    try:
-        for message in event['Records']:
-            logger.debug(f"Mensagem recebida: {message}")
-            response = process_message(message)
-
-            if response['statusCode'] not in [200, 201, 202]:
-                logger.error(f"Erro ao processar mensagem: {response}")
-                to_address = message['body']['to_address']
-                send_email_error(to_address)
-            else:
-                logger.info(f"Mensagem processada com sucesso: {response}")
-
-        return {
-            'statusCode': 200,
-            'body': json.dumps('Processamento concluído com sucesso.')
-        }
-    except Exception as e:
-        logger.exception("Erro durante o processamento do evento.")
-        return {
-            'statusCode': 500,
-            'body': json.dumps(f"Erro durante o processamento: {str(e)}")
-        }
-
-def process_message(message):
-    try:
+    logger.info(f"event: {event}")
+    for message in event['Records']:
         body_message = json.loads(message['body'])
-        status = body_message['status']
-        destinatario = body_message['to_address']
+        process_message(body_message)
+        
+        receipt_handle = message['receiptHandle']
+        delete_message(receipt_handle)
 
-        if status == "sucesso":
-            logger.info(f"Processando mensagem de sucesso para {destinatario}")
-            url_download = body_message['url_download']
-            response = send_email_success(destinatario, url_download)
-        else:
-            logger.warning(f"Status não é 'sucesso' para {destinatario}. Enviando e-mail de erro.")
-            response = send_email_error(destinatario)
+def process_message(body_message):
+    status = body_message['status']
+    destinatario = body_message['to_address']
 
-        return response
-    except Exception as e:
-        logger.exception("Erro ao processar a mensagem.")
-        return {
-            'statusCode': 500,
-            'body': json.dumps(f"Erro ao processar a mensagem: {str(e)}")
-        }
+    if status == "sucesso":
+        url_download = body_message['url_download']
+        send_email_success(destinatario, url_download)
+    else:
+        send_email_error(destinatario)
+
+    return {"statusCode": 200}
 
 def send_email_success(destinatario, url_download):
     headers = {
@@ -72,53 +47,20 @@ def send_email_success(destinatario, url_download):
     data = {
         "sender": {"email": email_sender},
         "to": [{"email": destinatario}],
-        "subject": "Seu vídeo está pronto para download!",
-        "htmlContent": f"""
-        <html>
-        <body>
-            <div style="font-family: Arial, sans-serif; text-align: center;">
-                <img alt="Video Frame Pro Logo" style="width: 150px; margin-bottom: 20px;" />
-                <h1 style="color: #2E86C1;">Olá!</h1>
-                <p style="font-size: 16px; color: #333;">
-                    Seu vídeo foi processado com sucesso e está disponível para download.
-                </p>
-                <p>
-                    <a href="{url_download}" style="display: inline-block; padding: 10px 20px; color: white; background-color: #28a745; text-decoration: none; border-radius: 5px; font-size: 16px;">
-                        Clique aqui para baixar seu arquivo
-                    </a>
-                </p>
-                <p style="font-size: 14px; color: #555;">
-                    Obrigado por usar o Video Frame Pro! Se tiver dúvidas, entre em contato conosco.
-                </p>
-            </div>
-        </body>
-        </html>
-        """
+        "subject": "Video Frame Pro",
+        "htmlContent": f"<html><body><h1>Link para download do .zip: {url_download}</h1></body></html>"
     }
 
     try:
-        logger.info(f"Enviando e-mail de sucesso para {destinatario}...")
         response = requests.post(url_smtp, headers=headers, json=data)
 
         if response.status_code in [202, 201]:
-            logger.info(f"E-mail enviado com sucesso para {destinatario}.")
-            return {
-                'statusCode': response.status_code,
-                'body': json.dumps('E-mail enviado com sucesso!')
-            }
+            logger.info("✅ E-mail enviado com sucesso!")
         else:
-            logger.error(f"Erro ao enviar o e-mail para {destinatario}: {response.status_code}")
-            return {
-                'statusCode': response.status_code,
-                'body': json.dumps(f'Erro ao enviar o e-mail: {response.status_code}')
-            }
-
+            logger.info(f"❌ Erro ao enviar o e-mail: {response.status_code}")
     except Exception as e:
-        logger.exception(f"Erro ao enviar o e-mail para {destinatario}.")
-        return {
-            'statusCode': 500,
-            'body': json.dumps(f"Erro na requisição: {str(e)}")
-        }
+        logger.info(f"Erro na requisição: {str(e)}")
+        
 
 def send_email_error(destinatario):
     headers = {
@@ -130,29 +72,26 @@ def send_email_error(destinatario):
         "sender": {"email": email_sender},
         "to": [{"email": destinatario}],
         "subject": "Video Frame Pro",
-        "htmlContent": "<html><body><h1>Erro ao processar o vídeo</h1></body></html>"
+        "htmlContent": "<html><body><h1>Erro ao processar o video</h1></body></html>"
     }
 
     try:
-        logger.info(f"Enviando e-mail de erro para {destinatario}...")
         response = requests.post(url_smtp, headers=headers, json=data)
 
         if response.status_code in [200, 201, 202]:
-            logger.info(f"E-mail de erro enviado para {destinatario}.")
-            return {
-                'statusCode': response.status_code,
-                'body': json.dumps('E-mail de erro enviado.')
-            }
+            logger.info(f"✅ E-mail de erro enviado.")
         else:
-            logger.error(f"Erro ao enviar o e-mail de erro para {destinatario}: {response.status_code}")
-            return {
-                'statusCode': response.status_code,
-                'body': json.dumps(f"Erro ao enviar o e-mail de erro: {response.status_code}")
-            }
+            logger.info(f"❌ Erro ao enviar o e-mail de erro: {response.status_code}")
 
     except Exception as e:
-        logger.exception(f"Erro ao enviar o e-mail de erro para {destinatario}.")
-        return {
-            'statusCode': 500,
-            'body': json.dumps(f"Erro na requisição: {str(e)}")
-        }
+        logger.info("Erro na requisição:", str(e))
+
+def delete_message(receipt_handle):
+    try:
+        sqs_client.delete_message(
+            QueueUrl=queue_url,
+            ReceiptHandle=receipt_handle
+        )
+        logger.info(f"Mensagem deletada da fila.")
+    except Exception as e:
+        logger.info("Erro ao deletar a mensagem da fila:", str(e))
--- tests/test_sendemail.py
+++ tests/test_sendemail.py
@@ -3,75 +3,61 @@ import os
 import sys
 from unittest import TestCase
 from unittest.mock import MagicMock, patch
+from unittest import mock
+import boto3
 
 from botocore.exceptions import ClientError
+
+os.environ['email_api_key'] = 'mocked_api_key'
+os.environ['AWS_REGION'] = 'us-east-1'
+session = boto3.Session(region_name='your-region-name')
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 
 from src.sendemail.sendemail import (lambda_handler, process_message,
-                                     send_email_error, send_email_success)
-
+                                     send_email_error, send_email_success, delete_message)
 
 class TestLambdaFunction(TestCase):
 
+    @patch('boto3.client')
     @patch('src.sendemail.sendemail.process_message')
-    @patch('src.sendemail.sendemail.send_email_error')
-    def test_lambda_handler_email_success(self, mock_send_email_error, mock_process_message):
+    @patch('src.sendemail.sendemail.delete_message')
+    def test_lambda_handler(self, delete_message, mock_process_message, mock_boto_client):
         mock_process_message.return_value = {'statusCode': 200}
+        mock_boto_client.return_value = MagicMock()
         event = {
             'Records': [{
-                'body': {
+                'body': json.dumps({
                     'status': 'sucesso',
                     'to_address': 'test@example.com',
                     'url_download': 'http://example.com/download'
-                }
+                }),
+                'receiptHandle': 'mock_receipt_handle'
             }]
         }
         context = {}
         lambda_handler(event, context)
         mock_process_message.assert_called_once()
-        mock_send_email_error.assert_not_called()
-
-    @patch('src.sendemail.sendemail.process_message')
-    @patch('src.sendemail.sendemail.send_email_error')
-    def test_lambda_handler_email_error(self, mock_send_email_error, mock_process_message):
-        mock_process_message.return_value = {'statusCode': 400}
-        event = {
-            'Records': [{
-                'body': {
-                    'status': 'erro',
-                    'to_address': 'test@example.com',
-                    'url_download': 'http://example.com/download'
-                }
-            }]
-        }
-        context = {}
-        lambda_handler(event, context)
-        mock_process_message.assert_called_once()
-        mock_send_email_error.assert_called_once()
+        delete_message.assert_called_once()
 
     @patch('src.sendemail.sendemail.send_email_success')
     def test_process_message_success(self, mock_send_email_success):
-        message = {
-            'body': {
-                'status': 'sucesso',
-                'to_address': 'test@example.com',
-                'url_download': 'http://example.com/download'
-            }
+        body_message  = {
+            'status': 'sucesso',
+            'to_address': 'test@example.com',
+            'url_download': 'http://example.com/download'
         }
-        process_message(message)
+        process_message(body_message )
         mock_send_email_success.assert_called_once_with('test@example.com', 'http://example.com/download')
 
     @patch('src.sendemail.sendemail.send_email_error')
     def test_process_message_error(self, mock_send_email_error):
-        message = {
-            'body': {
-                'status': 'erro',
-                'to_address': 'test@example.com',
-                'url_download': 'http://example.com/download'
-            }
+        body_message = {
+            'status': 'erro',
+            'to_address': 'test@example.com',
+            'url_download': 'http://example.com/download'
         }
-        process_message(message)
+        process_message(body_message)
         mock_send_email_error.assert_called_once_with('test@example.com')
 
     @patch('src.sendemail.sendemail.requests.post')
@@ -95,17 +81,15 @@ class TestLambdaFunction(TestCase):
         mock_post.side_effect = Exception("Error")
         destinatario = "test@example.com"
         url_download = "http://example.com/download"
-        response = send_email_success(destinatario, url_download)
-        self.assertIn('statusCode', response)
-        self.assertNotEqual(response['statusCode'], 500)
+        send_email_success(destinatario, url_download)
 
     @patch('src.sendemail.sendemail.requests.post')
     def test_send_email_error_success(self, mock_post):
         mock_post.return_value.status_code = 200
         destinatario = "test@example.com"
         send_email_error(destinatario)
-        mock_post.assert_called_once()
 
+    @patch.dict(os.environ, {"email_api_key": "mocked_api_key"})
     @patch('src.sendemail.sendemail.requests.post')
     def test_send_email_error_error(self, mock_post):
         mock_post.return_value.status_code = 500
@@ -117,6 +101,19 @@ class TestLambdaFunction(TestCase):
     def test_send_email_error_exception(self, mock_post):
         mock_post.side_effect = Exception("Error")
         destinatario = "test@example.com"
-        response = send_email_error(destinatario)
-        self.assertIn('statusCode', response)
-        self.assertNotEqual(response['statusCode'], 500)
+        send_email_error(destinatario)
+
+    @patch('src.sendemail.sendemail.sqs_client.delete_message')
+    def test_delete_message_success(self, mock_delete_message):
+        mock_delete_message.return_value = MagicMock()
+        QueueUrl='MockedQueueUrl',
+        receipt_handle = 'mock_receipt_handle'
+        
+        delete_message(receipt_handle)
+
+    @patch('src.sendemail.sendemail.sqs_client.delete_message')
+    def test_delete_message_exception(self, mock_delete_message):
+        mock_delete_message.side_effect = Exception("Error")
+        receipt_handle = 'mock_receipt_handle'
+        
+        delete_message(receipt_handle)
